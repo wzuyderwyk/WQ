2df8e13fbf95f44227664f08241c464b48d548b8
Wietse Zuyderwyk <wzuyderwyk@idiosys.com>
2013-11-11 17:55:50 +0100

    WQEvents prototype: added comment on Contextual value objects

---
+++ include/WQEvents.js
@@ -0,0 +1,13 @@
+/** @author: Wietse Zuyderwyk */
+
+/* WQEvents. Part of GeneriQ WQ. */
+
+/*
+Contextual value object:
+
+{ $x_context: CONTEXT_OBJECT_NUMBER_AS_WEAK_REFERENCE,
+  $x_condition: CONTEXT_OBJECT_CONDITION_REFERENCE,
+  handler: FUNCTION,
+  $x_default: CONTEXTUAL_VALUE_DEFAULT }
+
+*/
